e97d747fa63e6a0cd218d250de4c7722926ef909
praveen das <praveendask97@gmail.com>
2023-05-25 20:38:05 +0530

    update250323

--- src/lib/user.api.js
+++ src/lib/user.api.js
@@ -6,7 +6,7 @@ const signupUser = async (credentials) => {
 
 const signinUser = async (credentials) => {
   const data = await axiosClient.post("/user/signin", credentials);
-  console.log(data.headers.get('set-cookie'));
+  console.log(data);
   return data
 };
 
