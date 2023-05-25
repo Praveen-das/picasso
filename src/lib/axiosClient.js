d9ed0c35026b6372ba80f8cf429dd95a91bf695d
praveen das <praveendask97@gmail.com>
2023-05-25 21:08:53 +0530

    base url change

--- src/lib/axiosClient.js
+++ src/lib/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://artworld-server.vercel.app/";
+// const BASE_URL = "https://artworld-server.vercel.app/";
 // const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://artsworld-server.onrender.com";
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
